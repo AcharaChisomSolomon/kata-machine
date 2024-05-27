379a42ec0d4d0c0bf3b09ca9098f30540aecb4b4
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2024-05-27 19:39:30 +0100

    two crystal understanding solidified

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,19 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let step = Math.floor(Math.sqrt(breaks.length))
+    let start = 0
 
+    while (start < breaks.length) {
+        if (breaks[start]) {
+            break
+        }
+        start += step
+    }
+
+    for (let i = start - step + 1; i < breaks.length; i++) {
+        if (breaks[i]) {
+            return i
+        }
+    }
+
+    return -1
 }
