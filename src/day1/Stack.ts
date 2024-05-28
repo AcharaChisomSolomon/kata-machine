f13be3ece8005c294c7db1f4d72751b30586b598
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2024-05-28 18:40:12 +0100

    stack implemented

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,13 +1,35 @@
+type StackNode<T> = {
+    value: T;
+    next: StackNode<T> | undefined;
+};
+
 export default class Stack<T> {
     public length: number;
+    private head: StackNode<T> | undefined;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
-    push(item: T): void { }
+    push(item: T): void { 
+        const newNode = { value: item, next: this.head };
+        this.head = newNode;
+        this.length++;
+    }
     
-    pop(): T | undefined { }
+    pop(): T | undefined {
+        if (this.head === undefined) {
+            return undefined;
+        }
+        const value = this.head.value;
+        this.head = this.head.next;
+        this.length--;
+        return value;
+    }
     
-    peek(): T | undefined { }
+    peek(): T | undefined {
+        return this.head?.value;
+    }
     
 }
