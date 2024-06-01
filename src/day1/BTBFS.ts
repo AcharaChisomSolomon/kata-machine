2cfe2f99ee2e30538c2bfcbb582638eb349ac88e
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2024-06-01 07:45:50 +0100

    feat: Implement breadth-first search algorithm

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,19 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const queue = [head]
 
+    while (queue.length) {
+        const node = queue.shift() as BinaryNode<number>
+        if (node.value === needle) {
+            return true
+        }
+
+        if (node.left) {
+            queue.push(node.left)
+        }
+        if (node.right) {
+            queue.push(node.right)
+        }
+    }
+    
+    return false
 }
