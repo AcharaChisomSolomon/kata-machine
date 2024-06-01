813e340aef70462d908bab5db5be2d8e1dab92d0
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2024-06-01 08:14:26 +0100

    refactor: Update DFSOnBST to handle null head parameter

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,5 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
-
+export default function dfs(head: BinaryNode<number> | null, needle: number): boolean {
+    if (head === null) return false;
+    if (head.value === needle) return true;
+    return dfs(head.left, needle) || dfs(head.right, needle);
 }
