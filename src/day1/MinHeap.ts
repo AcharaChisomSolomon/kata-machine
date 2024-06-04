8c05a496e37e51e90a51d4500bc8ce283e87a947
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2024-06-04 08:42:49 +0100

    refactor: Improve delete method in MinHeap class

--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -13,7 +13,38 @@ export default class MinHeap {
         this.heapify();
     }
     
-    delete(): number {}
+    delete(): number {
+        if (this.length === 0) {
+            return -1;
+        }
+        const deletedValue = this.heap[0];
+        this.heap[0] = this.heap[this.length - 1];
+        this.heap.pop();
+        this.length--;
+        this.heapifyDown();
+        return deletedValue;
+    }
+
+    private heapifyDown(): void {
+        let index = 0;
+        while (index < this.length) {
+            const leftChildIndex = this.getLeftChildIndex(index);
+            const rightChildIndex = this.getRightChildIndex(index);
+            if (leftChildIndex >= this.length) {
+                break;
+            }
+            let smallerChildIndex = leftChildIndex;
+            if (rightChildIndex < this.length && this.heap[rightChildIndex] < this.heap[leftChildIndex]) {
+                smallerChildIndex = rightChildIndex;
+            }
+            if (this.heap[index] > this.heap[smallerChildIndex]) {
+                this.swap(index, smallerChildIndex);
+                index = smallerChildIndex;
+            } else {
+                break;
+            }
+        }
+    }
     
     private heapify(): void { 
         let index = this.length - 1;
