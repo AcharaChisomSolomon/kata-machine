ad9919e974202c27231372e977a40de1d3d985e4
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2024-06-05 12:10:28 +0100

    refactor: Improve BFS algorithm implementation

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,28 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+function walk(graph: WeightedAdjacencyMatrix, source: number, needle: number, visited: boolean[], walkedPath: number[]): number[] | null {
+    if (source === needle) {
+        return walkedPath;
+    }
 
+    visited[source] = true;
+
+    for (let i = 0; i < graph[source].length; i++) {
+        const weight = graph[source][i];
+
+        if (weight !== 0) {
+            const to = i;
+            if (!visited[to]) {
+                const path = walk(graph, to, needle, visited, [...walkedPath, to]);
+                if (path) {
+                    return path;
+                }
+            }
+        }
+    }
+
+    return null;
+}
+
+export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+    const visited = Array(graph.length).fill(false);
+    return walk(graph, source, needle, visited, [source]);
 }
--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,25 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(graph: WeightedAdjacencyList, source: number, needle: number, visited: boolean[], walkedPath: number[]): number[] | null {
+    if (source === needle) {
+        return walkedPath;
+    }
 
+    visited[source] = true;
+    const children = graph[source];
+
+    for (let i = 0; i < children.length; i++) {
+        const child = children[i];
+        if (!visited[child.to]) {
+            const path = walk(graph, child.to, needle, visited, [...walkedPath, child.to]);
+            if (path) {
+                return path;
+            }
+        }
+    }
+
+    return null;
+}
+
+export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+    const visited = Array(graph.length).fill(false);
+    return walk(graph, source, needle, visited, [source]);
 }
